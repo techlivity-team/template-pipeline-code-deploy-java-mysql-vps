fcb5e596cdfa20b83154c7da2dce7688e508b5ff
techlivity <techlivity@gmail.com>
2025-04-21 11:57:02 -0300

    update workflow

--- app/src/main/java/br/com/api/api_teste_mysql/controller/UserController.java
+++ app/src/main/java/br/com/api/api_teste_mysql/controller/UserController.java
@@ -2,6 +2,8 @@ package br.com.api.api_teste_mysql.controller;
 
 import br.com.api.api_teste_mysql.model.User;
 import br.com.api.api_teste_mysql.repository.UserRepository;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +27,17 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @GetMapping("/test")
-    public String test() {
+    @GetMapping("/{variable}")
+    public String test(@PathVariable(name = "variable") String variable) {
+        if (variable.equals("error")) {
+            throw new RuntimeException("Error");
+        }
+        MDC.put("correlationID", variable);
+        LoggerFactory.getLogger("teste")
+                .atInfo()
+                .addKeyValue("variable", variable)
+                .setMessage("validando teste")
+                .log();
         return "ok";
     }
 }
